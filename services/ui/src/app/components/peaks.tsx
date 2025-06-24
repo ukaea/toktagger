c17ab94c8319c6d068cae1b7a32f9f43187b3bbc
Samuel Jackson <samuel.jackson@ukaea.ac.uk>
2025-06-24 08:20:14 +0100

    Add types to states in findpeakstool

--- services/ui/src/app/components/peaks.tsx
+++ services/ui/src/app/components/peaks.tsx
@@ -1,15 +1,15 @@
 "use client";
-import {Provider, defaultTheme, Slider, Button, Flex, Header, ComboBox, Item, ToggleButton, RangeSlider} from '@adobe/react-spectrum'
+import {Provider, defaultTheme, Slider, Button, Flex, Header, ComboBox, Item, RangeSlider} from '@adobe/react-spectrum'
 import { useEffect, useState } from 'react';
 
 export function FindPeaksTool({ project_id, sample_id, data, setAnnotations }) {
     const [prominence, setProminance] = useState(0.1);
     const [distance, setDistance] = useState(1);
 
-    const [timeMinDefault, setTimeMinDefault] = useState(null);
-    const [timeMaxDefault, setTimeMaxDefault] = useState(null);
-    const [timeRange, setTimeRange] = useState({start: 0, end: 100}); 
-    const [signalName, setSignalName] = useState(null);
+    const [timeMinDefault, setTimeMinDefault] = useState<number>(null);
+    const [timeMaxDefault, setTimeMaxDefault] = useState<number>(null);
+    const [timeRange, setTimeRange] = useState<Record<string, number>>({start: 0, end: 100}); 
+    const [signalName, setSignalName] = useState<string>(null);
     const signalOptions = Object.keys(data.values).map((value, index)=> ({id: index, name: value}));
 
     const clearPeaks = () => {
@@ -17,6 +17,10 @@ export function FindPeaksTool({ project_id, sample_id, data, setAnnotations }) {
     };
 
     useEffect(() => {
+        if (signalName == null) {
+            return;
+        }
+
         if (data && (signalName in data.values)) {
             const time = data.values[signalName].time;
             const tmin = Math.min(...time);
