99f03f1587726e1a790bd08924b997802af3b42b
Matt Field <matthew.field@ukaea.uk>
2025-07-16 11:30:54 +0100

    Finished parametrized invalid ID tests

--- tests/api/routers/test_common.py
+++ tests/api/routers/test_common.py
@@ -1,25 +1,12 @@
 """Tests which are common to multiple endpoints which can be parametrized.
 """
 import pytest
-import asyncio
+import pytest_asyncio
 from bson.objectid import ObjectId
 from tests.db_definitions import SAMPLE
 
-# Provide the endpoint you want to be testing
-# Anywhere that you want an id inserted, put 'id'
-# Then enter the type of object you want tested, eg 'project'
-# The code will then figure out where to place correct and incorrect IDs
-# Also define the method ('get', 'post', 'put', 'delete') to use,
-# and if required provide a valid request body as a dict
-@pytest.mark.parametrize("endpoint, test_component, request_method, request_body", [
-    ("/projects/id", "project", "get", {}),
-    ("/projects/id/samples/id", "project", "get", {}),
-    ("/projects/id/samples/id", "sample", "get", {}),
-    ("/projects/id/samples", "project", "post", SAMPLE.model_dump(mode="python")),
-    ("/projects/id/samples", "sample", "post", SAMPLE.model_dump(mode="python")),
-])
-@pytest.mark.asyncio
-async def test_wrong_id(api_client, db_all, endpoint, test_component, request_method, request_body):
+def create_endpoint(endpoint: str, test_component: str, db_ids: dict, invalid_obj_id: bool):
+    """Create valid endpoint by inserting either valid or invalid IDs into their relevant positions."""
     # Split endpoint up
     components = endpoint.split("/")[1:]
     # Loop through each item in endpoint, check if it needs an ID replacing
@@ -28,52 +15,61 @@ async def test_wrong_id(api_client, db_all, endpoint, test_component, request_me
             continue
         # If this is the component being tested, insert incorrect object ID
         if components[i] == test_component + "s":
-            components[i+1] = str(ObjectId())
+            components[i+1] = "invalid_id" if invalid_obj_id else str(ObjectId())
         # Otherwise, insert valid ID
         else:
-            components[i+1] = db_all[components[i]]
+            components[i+1] = db_ids[components[i]]
             
-    endpoint_with_ids = "/".join(components)
+    return "/".join(components)
     
+async def make_request(api_client, request_method, endpoint, request_body):
+    """Make the request using the method defined in the parametrization, passing in request body if post/put."""
     if request_method == "get":
-        response = await api_client.get(endpoint_with_ids)
+        response = await api_client.get(endpoint)
     elif request_method == "put":
-        response = await api_client.put(endpoint_with_ids, json=request_body)
+        response = await api_client.put(endpoint, json=request_body)
     elif request_method == "post":
-        response = await api_client.post(endpoint_with_ids, json=request_body)
+        response = await api_client.post(endpoint, json=request_body)
     elif request_method == "delete":
-        response = await api_client.delete(endpoint_with_ids)
+        response = await api_client.delete(endpoint)
     else:
-        pytest.fail("Test setup failed: invalid request method.")
-        
-    assert response.status_code == 404
-    assert f"{test_component.title()} not found" in response.json().get("detail")
-    
-# The way this works is you should provide the endpoint you want to be testing
+        response = None
+    return response
+
+# To add to this test, provide the endpoint you want to be testing
 # Anywhere that you want an id inserted, put 'id'
 # Then enter the type of object you want tested, eg 'project'
 # The code will then figure out where to place correct and incorrect IDs
-@pytest.mark.parametrize("endpoint, test_component", [
-    ("/projects/id", "project"),
-    ("/projects/id/samples/id", "project"),
-    ("/projects/id/samples/id", "sample"),
-])
+# Also define the method ('get', 'post', 'put', 'delete') to use,
+# and if required provide a valid request body as a dict
+@pytest.mark.parametrize(
+    "endpoint, test_component, request_method, request_body", [
+        ("/projects/id", "project", "get", {}),
+        ("/projects/id/samples/id", "project", "get", {}),
+        ("/projects/id/samples/id", "sample", "get", {}),
+        ("/projects/id/samples", "project", "get", {}),
+        ("/projects/id/samples", "project", "put", [SAMPLE.model_dump(mode='json'),]),
+    ],
+    ids=[
+        "get_projects-wrong_project_id", 
+        "get_sample-wrong_project_id", 
+        "get_sample-wrong_sample_id",
+        "get_samples-wrong_project_id",
+        "put_samples-wrong_project_id"
+        ]
+    )
+@pytest.mark.parametrize("invalid_obj_id", (True, False), ids=["invalid_obj_id", "valid_obj_id"])
 @pytest.mark.asyncio
-async def test_invalid_id(api_client, db_all, endpoint, test_component):
-    # Use an ID which cannot be cast to an ObjectID correctly
-    # This error should be caught and raised as an appropriate HTTP response
-    components = endpoint.split("/")[1:]
-    # Loop through each item in endpoint, check if it needs an ID replacing
-    for i in range(0, len(components)-1, 2):
-        if components[i+1] != "id":
-            continue
-        # If this is the component being tested, insert incorrect object ID
-        if components[i] == test_component + "s":
-            components[i+1] = "invalid_id"
-        # Otherwise, insert valid ID
-        else:
-            components[i+1] = db_all[components[i]]
-            
-    response = await api_client.get("/".join(components))
-    assert response.status_code == 400
-    assert 'ID is not valid' in response.json().get("detail")
+async def test_invalid_id(api_client, db_all, endpoint, test_component, request_method, request_body, invalid_obj_id):
+    
+    endpoint_with_ids = create_endpoint(endpoint, test_component, db_all, invalid_obj_id)
+    response = await make_request(api_client, request_method, endpoint_with_ids, request_body)
+    if not response:
+        pytest.fail("Test setup failed: invalid request method.")
+    
+    if invalid_obj_id:
+        assert response.status_code == 400
+        assert f'{test_component.title()} ID is not valid' in response.json().get("detail")
+    else:
+        assert response.status_code == 404
+        assert f"{test_component.title()} not found" in response.json().get("detail")
