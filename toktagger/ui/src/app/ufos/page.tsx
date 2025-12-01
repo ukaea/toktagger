484652bc09325026357828af62ab91fb3127c447
Matt Field <matthew.field@ukaea.uk>
2025-12-01 15:30:43 +0000

    Correct page.tsx's

--- toktagger/ui/src/app/ufos/page.tsx
+++ toktagger/ui/src/app/ufos/page.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
     <div>
-      <h1>Locked mode</h1>
+      <h1>UFO tracking</h1>
       <p>Please enter a shot ID in the sidebar to get started</p>
     </div>
   );
