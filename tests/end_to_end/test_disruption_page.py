ec1d5e03acc39434c9ce8d1847dcf01af93d714a
Matt Field <matthew.field@ukaea.uk>
2026-01-05 10:00:06 +0000

    Add dragging zone test

--- tests/end_to_end/test_disruption_page.py
+++ tests/end_to_end/test_disruption_page.py
@@ -18,7 +18,7 @@ def test_disruption_add_zone(zone_type, server_setup, page: Page):
 
     sample_id = ids[0]
 
-    # Check I've navigated to the correct page
+    # Navigate to page
     page.goto(f"http://localhost:8002/ui/projects/{project_id}/samples/{sample_id}")
 
     # Check time series plot rendered
@@ -51,3 +51,72 @@ def test_disruption_add_zone(zone_type, server_setup, page: Page):
     # Check it no longer exists
     expect(page.locator(".zone").first).to_be_hidden()
     expect(page.get_by_role("rowheader", name=zone_type)).to_be_hidden()
+
+
+@pytest.mark.parametrize("zone_type", ["RampUp", "FlatTop", "RampDown"])
+@pytest.mark.parametrize("handle", ["leftHandle", "rightHandle"])
+@pytest.mark.parametrize("drag_to", [".wdrag", ".edrag"])
+def test_disruption_drag_zone(zone_type, handle, drag_to, server_setup, page: Page):
+    # Create Project
+    project_id = create_project("Test Project", "disruption", "parquet")
+    # And a sample for disruption
+    ids = create_local_samples(
+        project_id, [10000], pathlib.Path(__file__).parents[1], ["Ip"]
+    )
+
+    sample_id = ids[0]
+
+    # Navigate to page
+    page.goto(f"http://localhost:8002/ui/projects/{project_id}/samples/{sample_id}")
+
+    # Check time series plot rendered
+    expect(page.get_by_label("time-series")).to_be_visible()
+
+    # Add a new zone
+    page.get_by_label("time-series").click(button="right")
+    page.get_by_role("menuitem", name="Add zone").click()
+
+    # Choose each type, check a new zone is added
+    page.get_by_role("menuitem", name=zone_type, exact=True).click()
+    expect(page.locator(".zone").first).to_be_visible()
+
+    # Check added to list, record initial positions
+    expect(page.get_by_role("rowheader", name=zone_type)).to_be_visible()
+    initial_left_position = (
+        page.get_by_role("row").nth(1).get_by_role("cell").nth(0).inner_text()
+    )
+    initial_right_position = (
+        page.get_by_role("row").nth(1).get_by_role("cell").nth(1).inner_text()
+    )
+
+    # Click handle, drag to new position
+    page.locator(f".zone.{handle}").drag_to(page.locator(drag_to))
+
+    # Check values in table correctly updated
+    updated_left_position = (
+        page.get_by_role("row").nth(1).get_by_role("cell").nth(0).inner_text()
+    )
+    updated_right_position = (
+        page.get_by_role("row").nth(1).get_by_role("cell").nth(1).inner_text()
+    )
+
+    if drag_to == ".wdrag":
+        # Dragging left, left position should have reduced
+        assert updated_left_position < initial_left_position
+
+        if handle == "leftHandle":
+            # If left handle dragged, right position should be unchanged
+            assert updated_right_position == initial_right_position
+        else:
+            # If right handle dragged, positions should 'swap', so right position should be where left position was previously
+            assert updated_right_position == initial_left_position
+    else:
+        # Dragging right, right position should be the increased
+        assert updated_right_position > initial_right_position
+
+        if handle == "leftHandle":
+            # If left handle dragged, positions should 'swap', so left position should be where right position was previously
+            assert updated_left_position == initial_right_position
+        else:
+            # If right handle dragged, left position should be unchanged
+            assert updated_left_position == initial_left_position
