26b2b5f2f6145a3b11a4c69e973e3d36166b6828
Matt Field <matthew.field@ukaea.uk>
2026-02-16 12:01:53 +0000

    Update query strategies

--- toktagger/api/core/query_strategy.py
+++ toktagger/api/core/query_strategy.py
@@ -1,7 +1,6 @@
 from abc import ABC
 from typing import Optional
-
-import numpy as np
+import random
 
 from toktagger.api.schemas.samples import Sample
 from toktagger.api.schemas.annotations import Annotation
@@ -23,6 +22,19 @@ class QueryStrategy(ABC):
         self.samples = samples
         self.annotations = annotations if annotations is not None else []
 
+    def _get_matching_sample(self, current_sample_id: str) -> int:
+        index = next(
+            (
+                i
+                for i, sample in enumerate(self.samples)
+                if sample.id == current_sample_id
+            ),
+            None,
+        )
+        if index is None:
+            raise RuntimeError("Current sample ID not found in the list of samples.")
+        return index
+
     def get_next_sample(self, visited_sample_ids: list[str]) -> Sample:
         """Get the next sample"""
         next_sample = next(
@@ -40,6 +52,26 @@ class SequentialQueryStrategy(QueryStrategy):
     Chooses the next sample from the ordered list of samples
     """
 
+    def get_next_sample(self, visited_sample_ids: list[str]) -> Sample:
+        """Get the next sample, by finding the index of the last visited sample and doing +1"""
+        if not visited_sample_ids:
+            # Pressed Jump to Shot, should recommend next non-validated sample
+            next_sample = next(
+                (sample for sample in self.samples if not sample.validated_annotations),
+                None,
+            )
+            # If all are validated, just return first sample in table
+            if not next_sample:
+                return self.samples[0]
+            return next_sample
+
+        # Find index of last visited sample in samples list
+        index = self._get_matching_sample(visited_sample_ids[-1])
+        next_index = index + 1
+        next_index = next_index % len(self.samples)
+
+        return self.samples[next_index]
+
 
 class RandomQueryStrategy(QueryStrategy):
     """Random query strategy
@@ -47,20 +79,28 @@ class RandomQueryStrategy(QueryStrategy):
     Randomly chooses a sample as the next one to show to the user
     """
 
+    def _random_shuffle_samples(self, samples: list[Sample], seed: int):
+        rng = random.Random(seed)
+        rng.shuffle(samples)
+        return samples
+
     def __init__(
         self,
         samples: list[Sample],
         annotations: Optional[list[Annotation]] = None,
         seed: int = 42,
     ):
-        super().__init__(samples, annotations)
-        # simply shuffle the samples at the start
-        # seed is used to ensure consistent shuffling between calls
-        self._random_shuffle_samples(seed)
+        # Get list of non-validated samples and shuffle
+        samples_nonvalidated = [
+            sample for sample in samples if not sample.validated_annotations
+        ]
+        self.samples = self._random_shuffle_samples(samples_nonvalidated, seed)
 
-    def _random_shuffle_samples(self, seed: int):
-        rng = np.random.default_rng(seed=seed)
-        self.samples = rng.permutation(self.samples)
+        # Then get validated samples and shuffle
+        samples_validated = [
+            sample for sample in samples if sample.validated_annotations
+        ]
+        self.samples += self._random_shuffle_samples(samples_validated, seed)
 
 
 class UncertaintyQueryStrategy(RandomQueryStrategy):
@@ -76,8 +116,8 @@ class UncertaintyQueryStrategy(RandomQueryStrategy):
         annotations: Optional[list[Annotation]] = None,
         seed: int = 42,
     ):
-        annotations_nonvalidated = [ann for ann in annotations if not ann.validated]
-        super().__init__(samples, annotations_nonvalidated, seed)
+        self.annotations = [ann for ann in annotations if not ann.validated]
+        samples = self._random_shuffle_samples(samples=samples, seed=seed)
 
         if self.annotations:
             self.annotations = sorted(
@@ -87,7 +127,7 @@ class UncertaintyQueryStrategy(RandomQueryStrategy):
             sample_ids = list(dict.fromkeys(sample_ids))
             # List of samples which have unvalidated annotations
             samples_nonvalidated = [
-                sample for sample in self.samples if not sample.validated_annotations
+                sample for sample in samples if not sample.validated_annotations
             ]
             # Sort in order of annotation uncertainty, samples with no annotations go last (randomised)
             self.samples = sorted(
@@ -98,7 +138,7 @@ class UncertaintyQueryStrategy(RandomQueryStrategy):
             )
             # Then add in samples which *are* validated at the end
             self.samples += [
-                sample for sample in self.samples if sample.validated_annotations
+                sample for sample in samples if sample.validated_annotations
             ]
 
 
