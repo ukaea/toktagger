2ecedf4bfbb13130826ffaac809c78a1387893b7
Matt <matthew.field@ukaea.uk>
2026-04-30 14:03:56 +0100

    Try speeding up compose

--- toktagger/api/run.py
+++ toktagger/api/run.py
@@ -1,16 +1,4 @@
-import uvicorn
-import os
-import toktagger.api.cli
-from toktagger.api.models import models_dependencies_installed
+from toktagger.api.main import Server
 
-if __name__ == "__main__":
-    if models_dependencies_installed():
-        toktagger.api.cli.server._setup_ray(
-            "http://0.0.0.0:8002", os.environ.get("MODEL_STORAGE")
-        )
-    uvicorn.run(
-        "toktagger.api.cli:app",
-        host="0.0.0.0",
-        port=8002,
-        reload=True if os.environ.get("RELOAD") == "true" else False,
-    )
+server = Server()
+server.run("0.0.0.0", 8002)
