f6f695394b6cd5d61e93768a036cbd08df7d6d1b
Matt <matthew.field@ukaea.uk>
2026-04-30 14:38:05 +0100

    Fix run

--- toktagger/api/run.py
+++ toktagger/api/run.py
@@ -1,4 +1,17 @@
-from toktagger.api.main import Server
+import uvicorn
+import os
+import toktagger.api.cli
+from toktagger.api.models import models_dependencies_installed
 
-server = Server()
-server.run("0.0.0.0", 8002)
+if __name__ == "__main__":
+    if models_dependencies_installed():
+        toktagger.api.cli.server._setup_ray(
+            "http://0.0.0.0:8002", os.environ.get("MODEL_STORAGE")
+        )
+    # Can't just use server.Run with reload=True since it must point to a module level import
+    uvicorn.run(
+        "toktagger.api.cli:app",
+        host="0.0.0.0",
+        port=8002,
+        reload=True if os.environ.get("RELOAD") == "true" else False,
+    )
