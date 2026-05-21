6c99c377fcd34b71cc116ac4ef0fbb04c454a8fa
Matt <matthew.field@ukaea.uk>
2026-05-21 16:39:10 +0100

    Add test for model types endpoint

--- tests/api/routers/test_meta.py
+++ tests/api/routers/test_meta.py
@@ -17,3 +17,24 @@ async def test_get_data_schema(api_client, setup_db):
     data = response.json()
     assert data["title"] == "ShotData"
     assert data == ShotData.model_json_schema()
+
+
+@pytest.mark.asyncio
+@pytest.mark.parametrize("task", ["time-series", "video"])
+async def test_get_model_types(api_client, setup_db, task):
+    response = await api_client.get(f"/meta/models?task={task}")
+    assert response.status_code == 200
+
+    data = response.json()
+    models_present = (
+        item in data
+        for item in [
+            "mock_timeseries_cnn",
+            "mock_params_timeseries_cnn",
+            "mock_disruption_cnn",
+        ]
+    )
+    if task == "time-series":
+        assert all(models_present)
+    else:
+        assert not any(models_present)
